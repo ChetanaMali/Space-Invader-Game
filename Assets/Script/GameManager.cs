227e2586213078397f13e8d3e2c12b4ccd48cf11
ChetanaMali <chetanamali9763@gmail.com>
2022-08-09 17:47:00 +0530

    Remove Some unimportant stuff

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,9 +8,9 @@ public class GameManager : MonoBehaviour
     private static GameManager Instance;      //create instance variable 
 
     //Sound Controller Variable
-    [SerializeField] AudioSource[] audioSource;
-    AudioSource shoot;
-    AudioSource enemydead;
+   // [SerializeField] AudioSource[] audioSource;
+    [SerializeField] AudioSource shoot;
+    [SerializeField] AudioSource enemydead;
 
     //Scoring Variable
     [SerializeField] int score = 0;
@@ -19,15 +19,10 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;     //create instance
     }
-    private void Start()
-    {
-        //SoundController
-        audioSource = GetComponents<AudioSource>();
-        shoot = audioSource[0];
-        enemydead = audioSource[1];
-    }
+    
     public void _ShootSound()
     {
+        //Method for play shoot sound when player shoot
         Debug.Log("Play Shoot Sound");
         shoot.Play();
     }
@@ -37,6 +32,7 @@ public class GameManager : MonoBehaviour
     }
     public void _EnemydeadSound()
     {
+        // Method For Play sound when enemy dead
         Debug.Log("Enemy Dead Sound");
         enemydead.Play();
     }
@@ -46,6 +42,7 @@ public class GameManager : MonoBehaviour
     }
     public void _Scoring()
     {
+        // Method for Score as enemy dead Score will increses
         Debug.Log("Score is: " + score + ".");
         score+=5;
         scoringText.text = " Score: " + score.ToString();
